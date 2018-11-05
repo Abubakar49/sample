52c1db927fdbfafd8d1b6cd435e480f37a40bbdc
Abubakar49 <43042995+Abubakar49@users.noreply.github.com>
2018-11-05 12:25:15 -0500

    edits

--- JS/main.js
+++ JS/main.js
@@ -17,31 +17,87 @@ else if(myColour=="yellow"){
 	console.log("get him something yellow");
 }
 else{
-console.log("that's not a colour")
+console.log("that's not a colour");
 
   }
 
 }
 
 function test2(){
-  	var age=prompt("how old are you?")
+  	var age=prompt("how old are you?");
 
   	if(age <14){
-  		alert("That's not too old")
+  		alert("That's not too old");
 
    }
 	else if(age==14){
-		alert("14 is a good age")
+		alert("14 is a good age");
 
 }
 	else if(age>14){
-		alert("your're older than I thought")
+		alert("your're older than I thought");
 
 }
 	else{
-	alert("Not a valid age")
+	alert("Not a valid age");
 
 }
+
+
+}
+
+function pattern1(){
+
+var i = 1
+
+
+
+for( var i=1; i <= 100; i++)
+
+{
+
+
+if (  i%3 === 0 && i%5 === 0)
+
+		{
+
+		console.log( "FizzBuzz");
+
+		}
+
+			else if (i%3==0)
+		{
+
+		console.log ("Fizz");
+		}
+
+		else if (i%5 ===0)
+
+		{
+
+		console.log("Buzz");
+		}
+
+		else
+
+		{
+			console.log(i);
+		}
+	}
+}
+
+function pattern2(){
+
+	var x,y,chr;
+	for(x=1; x <=6; x++)
+    {
+    for (y=1; y < x; y++)	
+    	{
+    	chr=chr+("*");
+    	}
+    	console.log(chr);
+    	chr="";
+		}
 
 
 }
