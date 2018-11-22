fc69ede5ba1a927511abd182ca40bc8b415b7388
Abubakar49 <43042995+Abubakar49@users.noreply.github.com>
2018-11-22 13:47:53 -0500

    number generator

--- JS/main.js
+++ JS/main.js
@@ -18,9 +18,9 @@ else if(myColour=="yellow"){
 else{
 console.log("that's not a colour");
 
-  }
+  }  
 
-}
+}  
 
 function test2(){
   	var age=prompt("how old are you?");
@@ -99,4 +99,66 @@ function pattern2(){
 	}
 
 
+}
+
+
+
+
+function yourGuess() {
+
+  var guess = document.getElementById("guess").value;
+  var guesses = document.getElementById("output");
+
+  if (guess == numToGuess) {
+    guesses.value = guesses.value + "\r" + "You have guessed correctly! (" + guess + ")";
+  } else if (guess > numToGuess) {
+    guesses.value = guesses.value + "\r" + "You guessing too high!(" + guess + ")";
+  } else {
+    guesses.value = guesses.value + "\r" + "You guessing too low!(" + guess + ")";
+  }
+}
+
+function showNumberToGuess() {
+
+  if (document.getElementById('show').checked) {
+    document.getElementById('numberToGuess').value = numToGuess;
+    document.getElementById('Show').style.display = 'inline';
+  } else {
+    document.getElementById('numberToGuess').value = '';
+    document.getElementById('Show').style.display = 'none';
+  }
+}
+
+
+function generateNumberToGuess(confirmIt) {
+  var guesses = document.getElementById("output");
+
+  if (confirmIt && !confirm('Restart game with new number?')) {
+    return;
+  }
+
+  guesses.value = '';
+  numToGuess = Math.floor(Math.random() * 500);
+  guesses.value = "New number generated.\n";
+
+
+  document.getElementById('numberToGuess').value = '';
+  document.getElementById('Show').style.display = 'none';
+}
+
+function showGuesses() {
+  var guesses = document.getElementById('guesses');
+  var btn = document.getElementById('showguesses');
+
+  if (guesses.style.display != 'block') {
+    guesses.style.display = 'block';
+    btn.value = 'Hide My Guesses';
+  } else {
+    guesses.style.display = 'none';
+    btn.value = 'Show My Guesses';
+  }
+}
+
+window.onload = function() {
+  generateNumberToGuess();
 }
